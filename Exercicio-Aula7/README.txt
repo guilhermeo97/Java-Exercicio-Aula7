38e941118a8f6233ae5ee8537ef917d01c2bd232
guilhermeo97 <55219644+guilhermeo97@users.noreply.github.com>
2023-03-05 17:34:42 -0300

    1.0
    ...

---
+++ Exercicio-Aula7/README.txt
@@ -0,0 +1,18 @@
+Crie uma classe chamado Pessoa física. Lembre-se de utilizar os atalhos com o 'Ctrl + .' (Control + Ponto)
+Adicione as seguintes propriedade como privadas:
+nome
+ultimo nome
+gênero
+idade
+CPF
+estado civil
+CEP
+
+adicione um getter e um setter para cada umas das propriedades.
+crie o construtor da classe
+de override em equals e hashCode
+crie o método toString
+no método setter do CEP, certifique-se que o CEP passado contenha 8 dígitos numéricos, se não lance uma exceção
+
+       BONUS:  no método setter do CPF verifique se o numero de CPF é valido baseado na seguintes regras descritas no link abaixo, em caso do CPF passado for incorreto retorne uma exceção:
+https://docs.google.com/document/d/1NoADGqu18V9dIqjL4ar5NpAvSJ0AmslFIF_YqKMdjao/edit?usp=sharing
--- Exercicio-Aula7/src/App.java
+++ Exercicio-Aula7/src/App.java
@@ -1,5 +1,129 @@
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        PessoaFisica guilherme = new PessoaFisica("Guilherme", "Oliveira", "Homem", "Casado", 25, 95074554, 65888312266);
+
     }
-} 
+}
+
+class PessoaFisica {
+    private String nome, ultimoNome, genero, estadoCivil;
+    private int idade, cep;
+    private long cpf;
+    
+
+    public PessoaFisica(String nome, String ultimoNome, String genero, String estadoCivil, int idade, int cep,
+            long cpf) {
+        this.nome = nome;
+        this.ultimoNome = ultimoNome;
+        this.genero = genero;
+        this.estadoCivil = estadoCivil;
+        this.idade = idade;
+        this.cep = cep;
+        this.cpf = cpf;
+    }
+
+
+    public String getNome() {
+        return nome;
+    }
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+    public String getUltimoNome() {
+        return ultimoNome;
+    }
+    public void setUltimoNome(String ultimoNome) {
+        this.ultimoNome = ultimoNome;
+    }
+    public String getGenero() {
+        return genero;
+    }
+    public void setGenero(String genero) {
+        this.genero = genero;
+    }
+    public String getEstadoCivil() {
+        return estadoCivil;
+    }
+    public void setEstadoCivil(String estadoCivil) {
+        this.estadoCivil = estadoCivil;
+    }
+    public int getIdade() {
+        return idade;
+    }
+    public void setIdade(int idade) {
+        this.idade = idade;
+    }
+    public int getCep() {
+        return cep;
+    }
+    public void setCep(int cep) {
+        this.cep = cep;
+    }
+    public long getCpf() {
+        return cpf;
+    }
+    public void setCpf(long cpf) {
+        this.cpf = cpf;
+    }
+
+
+    @Override
+    public int hashCode() {
+        final int prime = 31;
+        int result = 1;
+        result = prime * result + ((nome == null) ? 0 : nome.hashCode());
+        result = prime * result + ((ultimoNome == null) ? 0 : ultimoNome.hashCode());
+        result = prime * result + ((genero == null) ? 0 : genero.hashCode());
+        result = prime * result + ((estadoCivil == null) ? 0 : estadoCivil.hashCode());
+        result = prime * result + idade;
+        result = prime * result + cep;
+        result = prime * result + (int) (cpf ^ (cpf >>> 32));
+        return result;
+    }
+
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj)
+            return true;
+        if (obj == null)
+            return false;
+        if (getClass() != obj.getClass())
+            return false;
+        PessoaFisica other = (PessoaFisica) obj;
+        if (nome == null) {
+            if (other.nome != null)
+                return false;
+        } else if (!nome.equals(other.nome))
+            return false;
+        if (ultimoNome == null) {
+            if (other.ultimoNome != null)
+                return false;
+        } else if (!ultimoNome.equals(other.ultimoNome))
+            return false;
+        if (genero == null) {
+            if (other.genero != null)
+                return false;
+        } else if (!genero.equals(other.genero))
+            return false;
+        if (estadoCivil == null) {
+            if (other.estadoCivil != null)
+                return false;
+        } else if (!estadoCivil.equals(other.estadoCivil))
+            return false;
+        if (idade != other.idade)
+            return false;
+        if (cep != other.cep)
+            return false;
+        if (cpf != other.cpf)
+            return false;
+        return true;
+    }
+
+
+    @Override
+    public String toString() {
+        return "PessoaFisica [nome=" + nome + ", ultimoNome=" + ultimoNome + ", genero=" + genero + ", estadoCivil="
+                + estadoCivil + ", idade=" + idade + ", cep=" + cep + ", cpf=" + cpf + "]";
+    }
+}
